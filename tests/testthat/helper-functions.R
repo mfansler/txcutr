3afb96b5bd2badd27f0fbf50fdf1ae596535ec55
Mervin Fansler <mmfansler@gmail.com>
2021-09-28 19:21:52 -0400

    resolves issue #11

--- tests/testthat/helper-functions.R
+++ tests/testthat/helper-functions.R
@@ -3,3 +3,11 @@ expect_equal_applied <- function (x, y, fns) {
     expect_equal(fn(x), fn(y))
   }
 }
+
+get_distal_tx_name <- function (gr) {
+  if (all(strand(gr) == '+')) {
+    gr$tx_name[which.max(end(gr))]
+  } else {
+    gr$tx_name[which.min(start(gr))]
+  }
+}
